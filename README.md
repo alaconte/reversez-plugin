55bbe4bfda4432bdc8612f5859f3e8299971fa78
Kharitaniya <alaconte@vt.edu>
2022-09-15 14:29:38 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ReversEz
+A basic volume control effect plug-in with IGraphics GUI
---
+++ config.h
@@ -0,0 +1,54 @@
+#define PLUG_NAME "ReversEz"
+#define PLUG_MFR "TheMixTrough"
+#define PLUG_VERSION_HEX 0x00010000
+#define PLUG_VERSION_STR "1.0.0"
+#define PLUG_UNIQUE_ID 'iAMT'
+#define PLUG_MFR_ID 'MixT'
+#define PLUG_URL_STR "https://iplug2.github.io"
+#define PLUG_EMAIL_STR "spam@me.com"
+#define PLUG_COPYRIGHT_STR "Copyright 2022 The Mix Trough"
+#define PLUG_CLASS_NAME ReversEz
+
+#define BUNDLE_NAME "ReversEz"
+#define BUNDLE_MFR "TheMixTrough"
+#define BUNDLE_DOMAIN "com"
+
+#define SHARED_RESOURCES_SUBPATH "ReversEz"
+
+#define PLUG_CHANNEL_IO "1-1 2-2"
+
+#define PLUG_LATENCY 0
+#define PLUG_TYPE 0
+#define PLUG_DOES_MIDI_IN 0
+#define PLUG_DOES_MIDI_OUT 0
+#define PLUG_DOES_MPE 0
+#define PLUG_DOES_STATE_CHUNKS 0
+#define PLUG_HAS_UI 1
+#define PLUG_WIDTH 600
+#define PLUG_HEIGHT 600
+#define PLUG_FPS 60
+#define PLUG_SHARED_RESOURCES 0
+#define PLUG_HOST_RESIZE 0
+
+#define AUV2_ENTRY ReversEz_Entry
+#define AUV2_ENTRY_STR "ReversEz_Entry"
+#define AUV2_FACTORY ReversEz_Factory
+#define AUV2_VIEW_CLASS ReversEz_View
+#define AUV2_VIEW_CLASS_STR "ReversEz_View"
+
+#define AAX_TYPE_IDS 'IEF1', 'IEF2'
+#define AAX_TYPE_IDS_AUDIOSUITE 'IEA1', 'IEA2'
+#define AAX_PLUG_MFR_STR "Acme"
+#define AAX_PLUG_NAME_STR "ReversEz\nIPEF"
+#define AAX_PLUG_CATEGORY_STR "Effect"
+#define AAX_DOES_AUDIOSUITE 1
+
+#define VST3_SUBCATEGORY "Fx"
+
+#define APP_NUM_CHANNELS 2
+#define APP_N_VECTOR_WAIT 0
+#define APP_MULT 1
+#define APP_COPY_AUV3 0
+#define APP_SIGNAL_VECTOR_SIZE 64
+
+#define ROBOTO_FN "Roboto-Regular.ttf"
